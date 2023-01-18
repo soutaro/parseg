73d9eb72fcb024b0fe16b2823566618271d33b6f
Soutaro Matsumoto <matsumoto@soutaro.com>
2023-01-18 23:57:52 +0900

    bundle gem

---
+++ lib/parseg.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "parseg/version"
+
+module Parseg
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/parseg/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Parseg
+  VERSION = "0.1.0"
+end
